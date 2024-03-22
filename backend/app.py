1a41617fd4983b6e1287bf9717f3a2e7c9927a70
Matthew Mentis-Cort <mam692@cornell.edu>
2024-03-22 12:37:50 -0400

    Got printing to work
    - Got titles and other info to print to website

--- backend/app.py
+++ backend/app.py
@@ -14,13 +14,14 @@ os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..",os.curdir))
 current_directory = os.path.dirname(os.path.abspath(__file__))
 
 # Specify the path to the JSON file relative to the current script
-json_file_path = os.path.join(current_directory, 'init.json')
+json_file_path = os.path.join(current_directory, 'compressed_df.json')
 
 # Assuming your JSON data is stored in a file named 'init.json'
-with open(json_file_path, 'r') as file:
-    data = json.load(file)
-    episodes_df = pd.DataFrame(data['episodes'])
-    reviews_df = pd.DataFrame(data['reviews'])
+# with open(json_file_path, 'r') as file:
+    # data = json.load(file)
+    # user_df = pd.DataFrame(data['user'])
+    # age_occ_df = pd.DataFrame(data['age_occupation'])
+    # compressed_df = pd.DataFrame(data)
 
 app = Flask(__name__)
 CORS(app)
@@ -28,33 +29,18 @@ CORS(app)
 # Sample search using json with pandas
 def json_search(query):
     df = pd.read_csv("compressed_df.csv")
-    # print(df)
+    # # print(df)
+    print("compressed_df:")
+    print(df)
     lst_title = df["title"]
     title_inv_idx = analysis.build_title_inverted_index(lst_title)
     tok_inv_idx = analysis.build_token_inverted_index(lst_title, title_inv_idx)
     results = analysis.boolean_search(query, tok_inv_idx, len(lst_title))
-    # print(len(results))
-    # matches_filtered = df.iloc[i]['title']
-    # list of titles (strings)
-    matches_filtered = [df.iloc[i]['title'] for i in results]
-    
-    matches_pd = pd.DataFrame(matches_filtered)
-    print("HEREEEEEEEEs")
-    print(matches_pd) 
-    # template code
-    
-    # matches = []
-    # merged_df = pd.merge(episodes_df, reviews_df, left_on='id', right_on='id', how='inner')
-    # matches = merged_df[merged_df['title'].str.lower().str.contains(query.lower())]
-    # matches_filtered = matches[['title', 'descr', 'imdb_rating']]
-    # matches_filtered_json = matches_filtered.to_json(orient='records')
-        
-    # this return value needs to be a json!
-    json = matches_pd.to_json()
-    filename = '/backend/data.json'
-        json.dump(json, f, indent=4)
-    print(json)
-    return matches_pd.to_json()
+    matches_filtered = df.iloc[results]
+    matches_filtered = matches_filtered[['title','author','ban_info']]
+    jsonified = matches_filtered.to_json(orient='records')
+    print(jsonified)
+    return jsonified
 
 @app.route("/")
 def home():
@@ -63,8 +49,59 @@ def home():
 @app.route("/episodes")
 def episodes_search():
     text = request.args.get("title")
-    print(type(text))
+    # print(type(text))
     return json_search(text)
 
 if 'DB_NAME' not in os.environ:
     app.run(debug=True,host="0.0.0.0",port=5000)
+
+
+
+
+
+# import json
+# import os
+# from flask import Flask, render_template, request
+# from flask_cors import CORS
+# from helpers.MySQLDatabaseHandler import MySQLDatabaseHandler
+# import pandas as pd
+
+# # ROOT_PATH for linking with all your files. 
+# # Feel free to use a config.py or settings.py with a global export variable
+# os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..",os.curdir))
+
+# # Get the directory of the current script
+# current_directory = os.path.dirname(os.path.abspath(__file__))
+
+# # Specify the path to the JSON file relative to the current script
+# json_file_path = os.path.join(current_directory, 'init.json')
+
+# # Assuming your JSON data is stored in a file named 'init.json'
+# with open(json_file_path, 'r') as file:
+#     data = json.load(file)
+#     episodes_df = pd.DataFrame(data['episodes'])
+#     reviews_df = pd.DataFrame(data['reviews'])
+
+# app = Flask(__name__)
+# CORS(app)
+
+# # Sample search using json with pandas
+# def json_search(query):
+#     matches = []
+#     merged_df = pd.merge(episodes_df, reviews_df, left_on='id', right_on='id', how='inner')
+#     matches = merged_df[merged_df['title'].str.lower().str.contains(query.lower())]
+#     matches_filtered = matches[['title', 'descr', 'imdb_rating']]
+#     matches_filtered_json = matches_filtered.to_json(orient='records')
+#     return matches_filtered_json
+
+# @app.route("/")
+# def home():
+#     return render_template('base.html',title="sample html")
+
+# @app.route("/episodes")
+# def episodes_search():
+#     text = request.args.get("title")
+#     return json_search(text)
+
+# if 'DB_NAME' not in os.environ:
+#     app.run(debug=True,host="0.0.0.0",port=5000)
