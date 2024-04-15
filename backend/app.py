77e675fe7d99b503bfd6c777d41d760d4ae6d330
Matthew Mentis-Cort <mam692@cornell.edu>
2024-04-15 18:30:57 -0400

    Code restructure
    - Added new comments to show what kinds
    of functions are in each section in app.py
    - Moved author reorganize function to its
    own function
    - Added document argument to the search
    functions for separation of concerns

--- backend/app.py
+++ backend/app.py
@@ -35,7 +35,6 @@ FIELDS_TO_PRINT = ['title', 'authors', 'ban_info', 'genres', 'ratings', 'summary
 
 ### Search Helper Functions ###
 
-
 def build_ban_freq_dict(ban_info_str: str) -> dict:
     """
     Returns a dictionary of mappings of states to the
@@ -68,7 +67,7 @@ def build_ban_freq_dict(ban_info_str: str) -> dict:
 def build_new_ban_info_col(df_col):
     """
     Returns a list of strings to be used as the new column
-    for df_col "ban_info" column of the matched dataframe.
+    for df_col "ban_info" column of the matches dataframe.
 
     Precondition: ``df_col`` is a column of data from a dataframe.
 
@@ -94,102 +93,19 @@ def build_new_ban_info_col(df_col):
         new_lst.append(state_ban_info_str[:-2])
     return new_lst
 
-### Searches ###
-
-def boolean_sim_search(query):
+def build_new_authors_col(df_col):
     """
-    Returns a list of the most similar documents to the query.
-    
-    Similarity is determined using a boolean AND search between
-    the words of the query.
+    Returns a list of strings to be used as the new column
+    for df_col "authors" column of the matches dataframe.
+
+    The new authors column is a reordering of the original author
+    names from "lastname, firstname" to "firstname lastname"
+
+    Precondition: ``df_col`` is a column of data from a dataframe.
     """
-    df = pd.read_csv("data/finalized_books.csv")
-
-    pd_title = df['title']
-    title_inv_idx = analysis.build_doc_inverted_index(pd_title)
-    tok_inv_idx = analysis.build_token_inverted_index(pd_title, title_inv_idx)
-    results = analysis.boolean_search(query, tok_inv_idx, len(pd_title), NUM_RESULTS)
-    return results
-
-def edit_dist_search(query):
-    """
-    Returns a json of the most similar documents to the query.
-    
-    Similarity is determined using a boolean AND search between
-    the words of the query.
-    """
-    df = pd.read_csv("data/finalized_books.csv")
-
-    pd_title = df['title']
-    title_inv_idx = analysis.build_doc_inverted_index(pd_title)
-    tok_inv_idx = analysis.build_token_inverted_index(pd_title, title_inv_idx)
-    #results = analysis.boolean_search(query, tok_inv_idx, len(pd_title))
-    # print(pd_title)
-    results = analysis.edit_distance_search(query, pd_title, analysis.insertion_cost, analysis.deletion_cost, analysis.substitution_cost)
-    
-    results = analysis.get_titleidx(results,title_inv_idx)
-    return results
-
-def cossim_sim_search(query):
-    """
-    Returns a list of the most similar documents to the query.
-    
-    Similarity is determined using cosine similarity
-    between the query and the summaries of all the books.
-    """
-    df = pd.read_csv("data/finalized_books.csv")
-    
-    nan_variable = "summary"
-    df_cleaned = df[nan_variable].fillna('')
-    lst_blurb = df_cleaned
-    # reviews = df["reviews"].fillna('')
-    # print("len(reviews):")
-    # print(len(reviews))
-    # print("len(lst_blurb):")
-    # print(len(lst_blurb))
-
-    # cossim_results = analysis.get_doc_rankings(query, lst_blurb, NUM_RESULTS)
-    cossim_results = analysis.get_doc_rankings(query, lst_blurb, NUM_RESULTS)
-    # print("cossim_results:")
-    # print(cossim_results)
-    # print("here 2")
-    return cossim_results
-
-def svd_sim_search(query):
-    """
-    Returns a list of the most similar documents to the query.
-    
-    Similarity is determined using SVD similarity
-    between the query and the summaries of all the books.
-    """
-    df = pd.read_csv("data/finalized_books.csv")
-    nan_variable = "reviews"
-    df_cleaned = df[nan_variable].fillna('')
-    lst_reviews = df_cleaned
-    # genre_lst = df["genres"]
-    # for idx in range(len(lst_reviews)):
-    #     lst_reviews[idx] += genre_lst[idx]
-
-    docs_compressed_normed, words_compressed_normed, query_vec = analysis.svd_analysis(lst_reviews, query)
-    # MAKE SURE TO TRANSPOSE words_compressed_normed!!!
-    top_matches_lst = analysis.closest_docs_to_query(docs_compressed_normed, words_compressed_normed.T, query_vec, NUM_RESULTS)
-    return top_matches_lst
-
-def convert_to_json(matches_lst):
-    """
-    Returns the json of information for the documents in ``doc_lst``.
-
-    Each document will have information from the fields in ``FIELDS_TO_PRINT``.
-    """
-    df = pd.read_csv("data/finalized_books.csv")
-
-    matches_filtered = df.iloc[matches_lst]
-    matches_filtered = matches_filtered[FIELDS_TO_PRINT]
-
     a = []
 
-    for author in matches_filtered["authors"]:
-        print(author)
+    for author in df_col:
         new_a = author
         str_lst = author.split(",")
         if len(str_lst) == 2:
@@ -198,17 +114,107 @@ def convert_to_json(matches_lst):
             new_a = first + " " + last
         new_a = new_a.strip()
         a.append(new_a)
+
+    return a
+
+### Searches ###
+
+def boolean_sim_search(query, docs):
+    """
+    Returns a list of the most similar documents to the query
+    using boolean AND search.
     
-    matches_filtered["authors"] = a
+    Similarity is determined using a boolean AND search between
+    the words of the query.
+
+    Precondition: ``docs`` is an iterable object
+    """
+    title_inv_idx = analysis.build_doc_inverted_index(docs)
+    tok_inv_idx = analysis.build_token_inverted_index(docs, title_inv_idx)
+    results = analysis.boolean_search(query, tok_inv_idx, len(docs), NUM_RESULTS)
+    return results
+
+def edit_dist_search(query, docs):
+    """
+    Returns a list of the most similar documents to the query
+    using edit distance.
+    
+    Similarity is determined using a boolean AND search between
+    the words of the query.
+
+    Precondition: ``docs`` is an iterable object
+    """
+    title_inv_idx = analysis.build_doc_inverted_index(docs)
+    # tok_inv_idx = analysis.build_token_inverted_index(doc_lst, title_inv_idx)
+    results = analysis.edit_distance_search(query, docs, analysis.insertion_cost, analysis.deletion_cost, analysis.substitution_cost)
+    
+    results = analysis.get_titleidx(results,title_inv_idx)
+    return results
+
+def cossim_sim_search(query, docs):
+    """
+    Returns a list of the most similar documents to the query
+    using cosine similarity.
+    
+    Similarity is determined using cosine similarity
+    between the query and the summaries of all the books.
+
+    Precondition: ``docs`` is an iterable object
+    """
+    # reviews = df["reviews"].fillna('')
+    # print("len(reviews):")
+    # print(len(reviews))
+    # print("len(lst_blurb):")
+    # print(len(lst_blurb))
+
+    # cossim_results = analysis.get_doc_rankings(query, lst_blurb, NUM_RESULTS)
+    cossim_results = analysis.get_doc_rankings(query, docs, NUM_RESULTS)
+    # print("cossim_results:")
+    # print(cossim_results)
+    # print("here 2")
+    return cossim_results
+
+def svd_sim_search(query, docs):
+    """
+    Returns a list of the most similar documents to the query using SVD.
+    
+    Similarity is determined using SVD similarity
+    between the query and the summaries of all the books.
+
+    Precondition: ``docs`` is an iterable object
+    """
+    docs_compressed_normed, words_compressed_normed, query_vec = analysis.svd_analysis(docs, query)
+    # MAKE SURE TO TRANSPOSE words_compressed_normed!!!
+    top_matches_lst = analysis.closest_docs_to_query(docs_compressed_normed, words_compressed_normed.T, query_vec, NUM_RESULTS)
+    return top_matches_lst
+
+### Production Conversion ###
+
+def convert_to_json(matches_lst: list):
+    """
+    Returns the json of information for the documents in ``matches_lst``.
+
+    Each document will have information from the fields in ``FIELDS_TO_PRINT``.
+    """
+    df = pd.read_csv("data/finalized_books.csv")
+
+    matches_filtered = df.iloc[matches_lst]
+    matches_filtered = matches_filtered[FIELDS_TO_PRINT]
+    
+    # construct new columns for data that needs to be displayed differently
+    all_authors_info = matches_filtered['authors']
+    matches_filtered["authors"] = build_new_authors_col(all_authors_info)
 
     all_ban_info = matches_filtered['ban_info']
     matches_filtered['ban_info'] = build_new_ban_info_col(all_ban_info)
 
-    print("first rating:")
-    print(matches_filtered["ratings"])
+    # print("first rating:")
+    # print(matches_filtered["ratings"])
 
     jsonified = matches_filtered.to_json(orient='records')
     return jsonified
+
+### Funcions to be called in HTML ###
 
 def title_search(query, sim_measure_code):
     """
@@ -223,15 +229,17 @@ def title_search(query, sim_measure_code):
     - 0: boolean similarity between words of query
     - 1: edit distance similarity between titles of documents
     """
+    df = pd.read_csv("data/finalized_books.csv")
+
+    docs = df['title']
+
     matches_lst = []
     if sim_measure_code == 0:
-        matches_lst = boolean_sim_search(query)
-        print("boolean search")
+        matches_lst = boolean_sim_search(query, docs)
         if len(matches_lst) == 0:
-            matches_lst = edit_dist_search(query)
-            print("failed boolean search")
+            matches_lst = edit_dist_search(query, docs)
     if sim_measure_code == 1:
-        matches_lst = edit_dist_search(query)
+        matches_lst = edit_dist_search(query, docs)
     return convert_to_json(matches_lst)
 
 def theme_search(query, sim_measure_code):
@@ -247,11 +255,19 @@ def theme_search(query, sim_measure_code):
     - 0: cosine similarity: 
     - 1: SVD similarity
     """
+    df = pd.read_csv("data/finalized_books.csv")
+
+    cossim_docs = df["summary"].fillna('')
+    svd_docs = df["reviews"].fillna('')
+    # genre_lst = df["genres"]
+    # for idx in range(len(lst_reviews)):
+    #     lst_reviews[idx] += genre_lst[idx]
+
     matches_lst = []
     if sim_measure_code == 0:
-        matches_lst = cossim_sim_search(query)
+        matches_lst = cossim_sim_search(query, cossim_docs)
     if sim_measure_code == 1:
-        matches_lst = svd_sim_search(query)
+        matches_lst = svd_sim_search(query, svd_docs)
     return convert_to_json(matches_lst)
 
 @app.route("/")
